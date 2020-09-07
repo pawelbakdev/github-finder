bc12832587c89b4bfaa286dacb1451403966e588
pawel-bak-dev <pawel.bak.dev@gmail.com>
2020-09-07 21:25:25 +0200

    Components, props, propTypes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
-    const name = 'Jan Nowak';
-    const loading = false;
-    const showName = false;
-
     return (
       <div className='App'>
-        {loading ? <h4>Loading...</h4> : <h1>Hello from {showName && name}</h1>}
+        <Navbar />
+        <h1>Hello</h1>
       </div>
     );
   }

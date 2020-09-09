7e94f3a5bf9a304a8f2b2db8ed38d1bcee14ebfc
pawel-bak-dev <pawel.bak.dev@gmail.com>
2020-09-09 16:03:15 +0200

    Home & NotFound Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import About from './components/pages/About';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 
 import GithubState from './conetxt/github/GithubState';
 import AlertState from './conetxt/alert/AlertState';
@@ -21,18 +21,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
